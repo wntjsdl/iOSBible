06dd506a0ee164bd76a7b8e4311822970007b002
Jusun Kang <wntjs0305@gmail.com>
2022-06-12 01:16:50 +0900

    Part3.Ch03.03 complete

---
+++ Part3_iOS_Development_Basic/Ch3-List_and_Grid/03_01-StockRank/starter/03_01-StockRank/StockRank/StockRankViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StockRankViewController.swift
+//  StockRank
+//
+//  Created by JuSun Kang on 2022/06/12.
+//
+
+import UIKit
+
+class StockRankViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
