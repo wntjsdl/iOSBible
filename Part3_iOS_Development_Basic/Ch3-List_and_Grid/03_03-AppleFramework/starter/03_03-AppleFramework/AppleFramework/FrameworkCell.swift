33762a73f91a8b529c7bcb74d4e31fcd9bc2a949
Jusun Kang <wntjs0305@gmail.com>
2022-06-21 23:53:21 +0900

    Part3.Ch03.12 AppleFramework is in progress.

---
+++ Part3_iOS_Development_Basic/Ch3-List_and_Grid/03_03-AppleFramework/starter/03_03-AppleFramework/AppleFramework/FrameworkCell.swift
@@ -0,0 +1,19 @@
+//
+//  FrameworkCell.swift
+//  AppleFramework
+//
+//  Created by powerapp on 2022/06/21.
+//
+
+import UIKit
+
+class FrameworkCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    func configure(_ framework: AppleFramework) {
+        thumbnailImageView.image = UIImage(named: framework.imageName)
+        nameLabel.text = framework.name
+    }
+}
