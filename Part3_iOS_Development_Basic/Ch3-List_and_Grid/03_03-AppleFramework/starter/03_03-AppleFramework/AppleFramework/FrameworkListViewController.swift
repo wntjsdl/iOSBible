4ee9985d88cedd927c63a842cf59a90fca180375
Jusun Kang <wntjs0305@gmail.com>
2022-06-25 02:48:39 +0900

    Part3.Ch03.14 AppleFramework is in progress.

--- Part3_iOS_Development_Basic/Ch3-List_and_Grid/03_03-AppleFramework/starter/03_03-AppleFramework/AppleFramework/FrameworkListViewController.swift
+++ Part3_iOS_Development_Basic/Ch3-List_and_Grid/03_03-AppleFramework/starter/03_03-AppleFramework/AppleFramework/FrameworkListViewController.swift
@@ -9,21 +9,60 @@ import UIKit
 
 class FrameworkListViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let list: [AppleFramework] = AppleFramework.list
+    
+    
+    // Data, Presentation, Layout
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        if let flowlayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowlayout.estimatedItemSize = .zero
+        }
+        
+        collectionView.contentInset = UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16)
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension FrameworkListViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return list.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FrameworkCell", for: indexPath) as? FrameworkCell else {
+            return UICollectionViewCell()
+        }
+        
+        let framework = list[indexPath.item]
+        cell.configure(framework)
+        return cell
     }
-    */
+    
+}
 
+extension FrameworkListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let interItemSpacing: CGFloat = 10
+        let padding: CGFloat = 16
+        
+        let width = (collectionView.bounds.width - interItemSpacing * 3 - padding * 2) / 4
+        let height = width * 1.5
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
 }
