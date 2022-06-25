4bdf611a06e3070a4ace13a7b525981915401f1c
Jusun Kang <wntjs0305@gmail.com>
2022-06-26 02:34:57 +0900

    Part3.Ch04.03 InstaSearchView is in progress.

---
+++ Part3_iOS_Development_Basic/Ch4-Tabbar/04_01-InstaSearchView/starter/04-InstaSearchView/InstaSearchView/ResultCell.swift
@@ -0,0 +1,21 @@
+//
+//  ResultCell.swift
+//  InstaSearchView
+//
+//  Created by powerapp on 2022/06/26.
+//
+
+import UIKit
+
+class ResultCell: UICollectionViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        thumbnailImageView.image = nil
+    }
+    
+    func configure(_ imageName: String) {
+        thumbnailImageView.image = UIImage(named: imageName)
+    }
+}
---
+++ Part3_iOS_Development_Basic/Ch4-Tabbar/04_01-InstaSearchView/starter/04-InstaSearchView/InstaSearchView/SearchViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SearchViewController.swift
+//  InstaSearchView
+//
+//  Created by powerapp on 2022/06/26.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // Data, Presentation, Layout
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        if let flowlayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowlayout.estimatedItemSize = .zero
+        }
+    }
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 24
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else {
+            return UICollectionViewCell()
+        }
+        let imageName = "animal\(indexPath.item + 1)"
+        cell.configure(imageName)
+        return cell
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let interItemSpacing: CGFloat = 1
+        let width = (collectionView.bounds.width - interItemSpacing * 2) / 3
+        let height = width
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+}
