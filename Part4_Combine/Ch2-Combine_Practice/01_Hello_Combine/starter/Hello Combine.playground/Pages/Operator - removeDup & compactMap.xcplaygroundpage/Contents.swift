71e7f6a99e8ce6795ee2c38179f0bf6339f4e0b3
Jusun Kang <wntjs0305@gmail.com>
2022-07-12 00:29:51 +0900

    Part4.Ch02.09 Combine is in progress.

--- Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
+++ Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
@@ -7,17 +7,47 @@ var subscriptions = Set<AnyCancellable>()
 
 // removeDuplicates
 
+let words = "hey hey there! Mr Mr ?"
+    .components(separatedBy: " ")
+    .publisher
+
+words
+    .removeDuplicates()
+    .sink { value in
+        print(value)
+    }.store(in: &subscriptions)
+
 
 // compactMap
 
+let strings = ["a", "1.24", "3", "def", "45", "0.23"].publisher
+
+strings
+    .compactMap { Float($0) }
+    .sink { value in
+        print(value)
+    }.store(in: &subscriptions)
 
 
 // ignoreOutput
 
+let numbers = (1...10_000).publisher
+
+numbers
+    .ignoreOutput()
+    .sink(receiveCompletion: { print("Completed with: \($0)") },
+          receiveValue: { print($0) })
+    .store(in: &subscriptions)
 
 
 // prefix
 
+let tens = (1...10).publisher
+
+tens.prefix(2)
+    .sink(receiveCompletion: { print("Completed with: \($0)") },
+          receiveValue: { print($0) })
+    .store(in: &subscriptions)
 
 
 //: [Next](@next)
